8effecede36f314379c4dacd05bd6d2490d45027
ownCloud Jenkins <jenkins@owncloud.com>
2017-07-09 01:24:13 +0000

    [tx-robot] updated from transifex

---
+++ l10n/bg_BG.js
@@ -0,0 +1,24 @@
+OC.L10N.register(
+    "diagnostics",
+    {
+    "Users" : "Потребители",
+    "Diagnostics" : "Диагностика",
+    "Nothing (collecting but not used)" : "Нищо (събирана но не се използва)",
+    "Summary (one report per request)" : "Обобщение (един доклад на заявка)",
+    "All queries (summary, single queries with their parameters)" : "Всички заявки (обобщение, едичен доклат с параметрите му)",
+    "All events (summary, single events)" : "Всички събития (обобшение, единични събития)",
+    "Everything (summary, single queries with their parameters and events)" : "Всичко (обобщение, единични заявки с техните параметри и събития)",
+    "Enabling this ownCloud diagnostic module will result in collecting data about all queries and events in the system per request." : "Активирането на този ownCloud диагностичен модул ще доведе до събирането на информация за всички заявки в системата.",
+    "Collect data only after authentication of users:" : "Събиране на информация само след вписването на потребителите:",
+    "Please specify full user name for best search performance" : "Моля, задайте пълни потребителски име за най-добри резултати от търсенето",
+    "Allow collecting all data (including unauthenticated requests, debug mode)" : "Разрешение за събиране на всичката информация (включително не вписани заявки, режим на дебъгване)",
+    "What to log" : "Какво да се добавя в доклада",
+    "Decide what details should be included in the log file" : "Вземане на решение за каква информация да се включва във файла на доклада",
+    "Diagnostic Log" : "Диагностичен Доклад",
+    "Download logfile (%s)" : "Изтегляне на доклад (1%s)",
+    "Clean logfile" : "Изчистване на доклада",
+    "Log file is located by default in ./data/diagnostic.log" : "Диагностичния файл се намира в ./data/diagnostic.log по подразбиране",
+    "The logfile is empty!" : "Докладът е празен!",
+    "The logfile is bigger than 100 MB. Downloading it may take some time!" : "Log файла е по-голям от 100 MB. Изтеглянето му може да отнеме време!"
+},
+"nplurals=2; plural=(n != 1);");
---
+++ l10n/he.js
@@ -0,0 +1,24 @@
+OC.L10N.register(
+    "diagnostics",
+    {
+    "Users" : "משתמשים",
+    "Diagnostics" : "אבחון",
+    "Nothing (collecting but not used)" : "שום דבר (נאספים אבל לא שומשו)",
+    "Summary (one report per request)" : "סיכום (דוח אחד לבקשה)",
+    "All queries (summary, single queries with their parameters)" : "כל השאילתות (סיכום, שאילתות בודדות עם המשתנים שלהן)",
+    "All events (summary, single events)" : "כל האירועים (סיכום, אירועים בודדים)",
+    "Everything (summary, single queries with their parameters and events)" : "הכול (סיכום, שאילתות בודדות עם המשתנים שלהן ואירועים)",
+    "Enabling this ownCloud diagnostic module will result in collecting data about all queries and events in the system per request." : "הפעלת מודול האבחון של ownCloud יגרום לאיסוף מידע אודות כל השאילתות והאירועים במערכת לבקשה.",
+    "Collect data only after authentication of users:" : "איסוף מידע רק אחרי אימות של משתמשים:",
+    "Please specify full user name for best search performance" : "לטובת ביצועי חיפוש עדיפים יש לספק שם משתמש מלא",
+    "Allow collecting all data (including unauthenticated requests, debug mode)" : "מאפשר איסוף של כל המידע (כולל שאילתות לא מאומתות, מצב ניפוי שגיאות)",
+    "What to log" : "מה לנטר בלוג",
+    "Decide what details should be included in the log file" : "מחליט איזה מידע יהיה כלול בקובץ התיעוד",
+    "Diagnostic Log" : "קובץ תיעוד האבחון",
+    "Download logfile (%s)" : "הורדת קובץ לוג (%s)",
+    "Clean logfile" : "מנקה קובץ תיעוד",
+    "Log file is located by default in ./data/diagnostic.log" : "קובץ תיעוד ממוקם כברירת מחדל ב- ./data/diagnostic.log",
+    "The logfile is empty!" : "קובץ התיעוד ריק!",
+    "The logfile is bigger than 100 MB. Downloading it may take some time!" : "קובץ הלוג גדול מ- 100 מגה-בייט. הורדה עלולה לקחת זמן רב!"
+},
+"nplurals=2; plural=(n != 1);");
