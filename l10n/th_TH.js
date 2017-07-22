4074791bbc02bbee8c28d9130389351574942418
ownCloud Jenkins <jenkins@owncloud.com>
2017-07-22 01:24:16 +0000

    [tx-robot] updated from transifex

---
+++ l10n/th_TH.js
@@ -0,0 +1,24 @@
+OC.L10N.register(
+    "diagnostics",
+    {
+    "Users" : "ผู้ใช้งาน",
+    "Diagnostics" : "วินิจฉัย",
+    "Nothing (collecting but not used)" : "ไม่มี (เก็บแต่ไม่ได้ใช้)",
+    "Summary (one report per request)" : "สรุป (หนึ่งรายงานต่อหนึ่งคำขอ)",
+    "All queries (summary, single queries with their parameters)" : "รายการทั้งหมด (สรุปหรือรายการเดียวพร้อมด้วยพารามิเตอร์)",
+    "All events (summary, single events)" : "กิจกรรมทั้งหมด (สรุปหรือเหตุการณ์เดียว)",
+    "Everything (summary, single queries with their parameters and events)" : "ทุกทุกอย่าง (สรุปหรือรายการเดียวพร้อมด้วยพารามิเตอร์และเหตุการณ์)",
+    "Enabling this ownCloud diagnostic module will result in collecting data about all queries and events in the system per request." : "กำลังเปิดใช้งานโมดูลการวินิจฉัย ownCloud นี้จะมีผลในการรวบรวมข้อมูลเกี่ยวกับรายการและเหตุการณ์ทั้งหมดในระบบต่อหนึ่งคำขอ",
+    "Collect data only after authentication of users:" : "รวบรวมเฉพาะข้อมูลหลังจากการตรวจสอบสิทธิ์ของผู้ใช้:",
+    "Please specify full user name for best search performance" : "โปรดระบุชื่อเต็มของผู้ใช้เพื่อจะได้ผลลัพธ์การค้นหาที่ดีที่สุด",
+    "Allow collecting all data (including unauthenticated requests, debug mode)" : "อนุญาตให้รวบรวมข้อมูลทั้งหมด (รวมถึงคำขอที่ไม่ผ่านการรับรองความถูกต้อง, โหมดการแก้ปัญหา)",
+    "What to log" : "อะไรที่จะบันทึก",
+    "Decide what details should be included in the log file" : "ตัดสินใจว่าอะไรที่ควรจะบันทึกบ้าง",
+    "Diagnostic Log" : "บันทึกการวินิจฉัย",
+    "Download logfile (%s)" : "ดาวน์โหลดไฟล์บันทึก (%s)",
+    "Clean logfile" : "ล้างบันทึก",
+    "Log file is located by default in ./data/diagnostic.log" : "ค่าเริ่มต้นของบันทึกจะอยู่ที่ ./data/diagnostic.log",
+    "The logfile is empty!" : "ไม่มีบันทึก!",
+    "The logfile is bigger than 100 MB. Downloading it may take some time!" : "ไฟล์บันทึกมีขนาดใหญ่กว่า 100 เมกะไบต์ มันอาจจะใช้เวลาดาวน์โหลดนาน!"
+},
+"nplurals=1; plural=0;");
