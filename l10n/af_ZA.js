e7e6bbf7eac9b984b3074749a5725cd509e38b07
ownCloud Jenkins <jenkins@owncloud.com>
2017-08-25 01:24:27 +0000

    [tx-robot] updated from transifex

---
+++ l10n/af_ZA.js
@@ -0,0 +1,24 @@
+OC.L10N.register(
+    "diagnostics",
+    {
+    "Users" : "Gebruikers",
+    "Diagnostics" : "Diagnose",
+    "Nothing (collecting but not used)" : "Niks (samel in maar gebruik nie)",
+    "Summary (one report per request)" : "Opsomming (een verslag per versoek)",
+    "All queries (summary, single queries with their parameters)" : "Alle navrae (opsomming, enkelnavrae met hul parameters)",
+    "All events (summary, single events)" : "Alle gebeure (opsomming, enkelgebeure)",
+    "Everything (summary, single queries with their parameters and events)" : "Alles (opsomming, enkelnavrae met hul parameters en gebeure)",
+    "Enabling this ownCloud diagnostic module will result in collecting data about all queries and events in the system per request." : "Aktivering van hierdie ownCloud-diagnosemodule sal lei tot die insamel van alle navrae en gebeure in die stelsel per versoek.",
+    "Collect data only after authentication of users:" : "Versamel slegs data na verifiëring van gebruikers:",
+    "Please specify full user name for best search performance" : "Spesifiseer volle gebruikersnaam vir die beste soekverrigting",
+    "Allow collecting all data (including unauthenticated requests, debug mode)" : "Staan datainsameling toe (insluitend nie-geverifieerde versoeke, ontfoutmodus)",
+    "What to log" : "Wat om te log",
+    "Decide what details should be included in the log file" : "Besluit welke besonderhede by die loglêer ingesluit moet word",
+    "Diagnostic Log" : "Diagnoselog",
+    "Download logfile (%s)" : "Laai loglêer af (%s)",
+    "Clean logfile" : "Ledig loglêer",
+    "Log file is located by default in ./data/diagnostic.log" : "Loglêer lê by verstek in ./data/diagnostic.log",
+    "The logfile is empty!" : "Die loglêer is leeg!",
+    "The logfile is bigger than 100 MB. Downloading it may take some time!" : "Die loglêer is groter as 100 MB. Dit kan dalk lank vat om af te laai!"
+},
+"nplurals=2; plural=(n != 1);");
