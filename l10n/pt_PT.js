59e52f42d02ba56f2dbcea7b607652c7f48dd861
ownCloud Jenkins <jenkins@owncloud.com>
2017-11-11 01:24:18 +0000

    [tx-robot] updated from transifex

---
+++ l10n/pt_PT.js
@@ -0,0 +1,20 @@
+OC.L10N.register(
+    "diagnostics",
+    {
+    "Users" : "Utilizadores",
+    "Diagnostics" : "Diagnósticos",
+    "Nothing (collecting but not used)" : "Nada (a recolher, mas sem estar a utilizar)",
+    "Summary (one report per request)" : "Resumo (um relatório por pedido)",
+    "All events (summary, single events)" : "Todos os eventos (resumo, um evento)",
+    "Collect data only after authentication of users:" : "Só obter os dados depois da autenticação dos utilizadores:",
+    "Please specify full user name for best search performance" : "Por favor especifique o nome completo do utilizador para melhor desempenho na procura.",
+    "What to log" : "O que registar",
+    "Decide what details should be included in the log file" : "Decida quais detalhes devem ser incluídos no ficheiro de registo",
+    "Diagnostic Log" : "Registo do Diagnóstico",
+    "Download logfile (%s)" : "Transferir ficheiro de registo (%s)",
+    "Clean logfile" : "Limpar ficheiro de registo",
+    "Log file is located by default in ./data/diagnostic.log" : "O ficheiro de registo está localizado por predefinição em ./data/diagnostic.log",
+    "The logfile is empty!" : "O ficheiro de registo está vazio!",
+    "The logfile is bigger than 100 MB. Downloading it may take some time!" : "O ficheiro de registo é maior do que 100MB. A sua transferência poderá demorar algum tempo!"
+},
+"nplurals=2; plural=(n != 1);");
