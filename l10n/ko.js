187f648168c7a8d7b5b45b5c2890339e401448d7
ownCloud Jenkins <jenkins@owncloud.com>
2018-01-06 01:24:34 +0000

    [tx-robot] updated from transifex

---
+++ l10n/ko.js
@@ -0,0 +1,24 @@
+OC.L10N.register(
+    "diagnostics",
+    {
+    "Users" : "사용자",
+    "Diagnostics" : "진단",
+    "Nothing (collecting but not used)" : "없음(수집하지만 사용하지 않음)",
+    "Summary (one report per request)" : "요약(요청당 보고 1회)",
+    "All queries (summary, single queries with their parameters)" : "모든 질의(요약, 매개 변수를 포함한 개별 질의)",
+    "All events (summary, single events)" : "모든 이벤트(요약, 개별 이벤트)",
+    "Everything (summary, single queries with their parameters and events)" : "전체(요약, 매개 변수를 포함한 개별 질의와 이벤트)",
+    "Enabling this ownCloud diagnostic module will result in collecting data about all queries and events in the system per request." : "ownCloud 진단 모듈을 활성화하면 시스템에 발생한 모든 요청마다 쿼리와 이벤트 데이터를 수집합니다.",
+    "Collect data only after authentication of users:" : "사용자 인증 후에만 데이터 수집:",
+    "Please specify full user name for best search performance" : "검색 성능을 향상하려면 전체 사용자 이름을 입력하십시오",
+    "Allow collecting all data (including unauthenticated requests, debug mode)" : "모든 데이터 수집 허용(인증되지 않은 요청 포함, 디버그 모드)",
+    "What to log" : "남길 로그",
+    "Decide what details should be included in the log file" : "로그 파일에 포함할 항목을 선택하십시오",
+    "Diagnostic Log" : "진단 로그",
+    "Download logfile (%s)" : "로그 파일 다운로드(%s)",
+    "Clean logfile" : "로그 파일 비우기",
+    "Log file is located by default in ./data/diagnostic.log" : "로그 파일은 기본적으로 ./data/diagnostic.log에 있습니다",
+    "The logfile is empty!" : "로그 파일이 비어 있습니다!",
+    "The logfile is bigger than 100 MB. Downloading it may take some time!" : "로그 파일이 100MB보다 큽니다. 다운로드하는 데 시간이 걸릴 수 있습니다!"
+},
+"nplurals=1; plural=0;");
