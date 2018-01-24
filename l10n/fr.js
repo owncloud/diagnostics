a65fd04e72e6b65690f7840509b19be72258bcd0
ownCloud Jenkins <jenkins@owncloud.com>
2018-01-24 02:27:58 +0000

    [tx-robot] updated from transifex

---
+++ l10n/fr.js
@@ -0,0 +1,24 @@
+OC.L10N.register(
+    "diagnostics",
+    {
+    "Users" : "Utilisateurs",
+    "Diagnostics" : "Diagnostics",
+    "Nothing (collecting but not used)" : "Néant (reçu mais inutilisé)",
+    "Summary (one report per request)" : "Résumé (un seul compte-rendu par demande)",
+    "All queries (summary, single queries with their parameters)" : "Toutes les recherches (résumés, recherches simples avec leurs paramètres)  ",
+    "All events (summary, single events)" : "Tous les évènements (résumé, événements uniques)",
+    "Everything (summary, single queries with their parameters and events)" : "Tous les résultats (résumés, recherches simples avec leurs paramètres et leurs résultats) ",
+    "Enabling this ownCloud diagnostic module will result in collecting data about all queries and events in the system per request." : "Définir que ce module de diagnostic ownCloud pourra collecter les données de toutes les requêtes et de tous les événements survenant dans le système pour chaque requête.",
+    "Collect data only after authentication of users:" : "Collecter les données seulement après authentification des utilisateurs:",
+    "Please specify full user name for best search performance" : "Veuillez entrer le nom complet de l'utilisateur pour améliorer le résultat de la recherche",
+    "Allow collecting all data (including unauthenticated requests, debug mode)" : "Autoriser la collecte de toutes les données (y compris les requêtes non-authentifiées ou effectuées en mode déboguage)",
+    "What to log" : "Ce qu'il faut journaliser",
+    "Decide what details should be included in the log file" : "Choix des détails à inclure dans le fichier journal",
+    "Diagnostic Log" : "Fichier journal de diagnostic",
+    "Download logfile (%s)" : "Télécharger le fichier journal (%s)",
+    "Clean logfile" : "Nettoyer le fichier log",
+    "Log file is located by default in ./data/diagnostic.log" : "Par défaut, le fichier journal est placé dans ./data/diagnostic.log",
+    "The logfile is empty!" : "Le fichier journal est vide",
+    "The logfile is bigger than 100 MB. Downloading it may take some time!" : "La taille du fichier journal excède 100 Mo. Le télécharger peut prendre un certain temps!"
+},
+"nplurals=2; plural=(n > 1);");
