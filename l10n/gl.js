370513fad1f8cf0f8e8fa09cc68be3a9df38a48f
ownClouders <devops@owncloud.com>
2019-01-20 05:05:03 +0000

    [tx] updated from transifex

---
+++ l10n/gl.js
@@ -0,0 +1,24 @@
+OC.L10N.register(
+    "diagnostics",
+    {
+    "Users" : "Usuarios",
+    "Diagnostics" : "Diagnósticos",
+    "Nothing (collecting but not used)" : "Nada (recopilados mais non empregados)",
+    "Summary (one report per request)" : "Resumo (un informe por solicitude)",
+    "All queries (summary, single queries with their parameters)" : "Todas as consultas (resumo, consultas individuais coa seus parámetros)",
+    "All events (summary, single events)" : "Todos os eventos (resumo, eventos individuais)",
+    "Everything (summary, single queries with their parameters and events)" : "Todo (resumo, consultas individuais coa seus parámetros e eventos)",
+    "Enabling this ownCloud diagnostic module will result in collecting data about all queries and events in the system per request." : "Activando este módulo de diagnóstico para o ownCloud recompilaranse datos sobre todas as consultas e eventos do sistema por solicitude. ",
+    "Collect data only after authentication of users:" : "Recompilar datos só após a autenticación dos usuarios:",
+    "Please specify full user name for best search performance" : "Especifique o nome completo do usuario para mellorar o rendemento da busca",
+    "Allow collecting data for all requests in debug mode (all users, unauthenticated requests)" : "Permitir a recompilación de datos de todas as solicitudes en modo depuración (todos os usuarios, solicitudes sen autenticación) ",
+    "What to log" : "Que rexistrar",
+    "Decide what details should be included in the log file" : "Decidir que detalles deben incluírse no ficheiro do rexistro",
+    "Diagnostic Log" : "Rexistro do diagnóstico",
+    "Download logfile (%s)" : "Descargar o ficheiro do rexistro (%s)",
+    "Clean logfile" : "Limpar o ficheiro do rexistro",
+    "Log file is located by default in ./data/diagnostic.log" : "O ficheiro do rexistro, de xeito predeterminado, atópase en ./data/diagnostic.log ",
+    "The logfile is empty!" : "O ficheiro do rexistro está baleiro",
+    "The logfile is bigger than 100 MB. Downloading it may take some time!" : "O ficheiro de rexistro é maior de 100 MB. Pódelle levar un anaco descargalo!"
+},
+"nplurals=2; plural=(n != 1);");
