0f9a5ad7b064e8ace268c1d6faa6a0913904acaa
ownClouders <devops@owncloud.com>
2019-08-31 01:18:05 +0000

    [tx] updated from transifex

---
+++ l10n/eu.js
@@ -0,0 +1,24 @@
+OC.L10N.register(
+    "diagnostics",
+    {
+    "Users" : "Erabiltzaileak",
+    "Diagnostics" : "Diagnostikoak",
+    "Nothing (collecting but not used)" : "Ezer (jasota baina erabili gabea)",
+    "Summary (one report per request)" : "Laburpena (eskari bakoitzeko txosten bat)",
+    "All queries (summary, single queries with their parameters)" : "Kontsulta guztiak (laburpena, kontsulta bakoitza bere parametroekin)",
+    "All events (summary, single events)" : "Gertaera guztiak (laburpena, gertaera bakoitza)",
+    "Everything (summary, single queries with their parameters and events)" : "Dena (laburpena, kontsulta bakoitza bere parametro eta gertaerekin)",
+    "Enabling this ownCloud diagnostic module will result in collecting data about all queries and events in the system per request." : "ownCloud diagnostiko modulu hau gaitzean eskaera bakoitzaren kontsulta eta gertaera guztien datuak bilduko dira",
+    "Collect data only after authentication of users:" : "Datuak jaso erabiltzaile hauen autentifikazioaren ondoren bakarrik:",
+    "Please specify full user name for best search performance" : "Erabiltzailearen izen osoa zehaztu bilaketa errendimendu hobea lortzeko",
+    "Allow collecting data for all requests in debug mode (all users, unauthenticated requests)" : "Eskaera guztien datu-jasotzea baimendu arazketa moduan (erabiltzaile guztiak, autentifikatu gabeko eskaerak)",
+    "What to log" : "Zer gorde",
+    "Decide what details should be included in the log file" : "Erabaki zein xehetasun gorde log fitxategian",
+    "Diagnostic Log" : "Diagnostiko Loga",
+    "Download logfile (%s)" : "Log fitxategia jaitsi (%s)",
+    "Clean logfile" : "Log fitxategia garbitu",
+    "Log file is located by default in ./data/diagnostic.log" : "Log fitxategi ./data/diagnostics.log kokapenean dago modu lehenetsian",
+    "The logfile is empty!" : "Log fitxategia hutsik dago!",
+    "The logfile is bigger than 100 MB. Downloading it may take some time!" : "Log fitxategiak 100 MB baina gehiago ditu. Berau jaisteak denbora eman dezake!"
+},
+"nplurals=2; plural=(n != 1);");
