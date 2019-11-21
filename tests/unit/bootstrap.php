4f977a0daa0d25c6c753b8ebd83ff7ad268f92ee
Phil Davis <phil@jankaritech.com>
2019-11-21 10:53:56 +0545

    Infrastructure for Makefile test targets

--- tests/unit/bootstrap.php
+++ tests/unit/bootstrap.php
@@ -4,14 +4,10 @@ if (!\defined('PHPUNIT_RUN')) {
 	\define('PHPUNIT_RUN', 1);
 }
 
-require_once __DIR__.'/../../../lib/base.php';
+require_once __DIR__ . '/../../../../lib/base.php';
 
 \OC::$composerAutoloader->addPsr4('Test\\', OC::$SERVERROOT . '/tests/lib/', true);
 
 \OC::$loader->addValidRoot(OC::$SERVERROOT . '/tests');
 
 \OC_App::loadApp('diagnostics');
-
-if (!\class_exists('PHPUnit_Framework_TestCase')) {
-	require_once('PHPUnit/Autoload.php');
-}
