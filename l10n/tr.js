d5599e6a8d7fc297c91bf9e4b50c5de20fc35b31
Dominik Schmidt <domme@tomahawk-player.org>
2020-01-24 02:31:39 +0000

    [tx] updated from transifex

---
+++ l10n/tr.js
@@ -0,0 +1,24 @@
+OC.L10N.register(
+    "diagnostics",
+    {
+    "Users" : "Kullanıcılar",
+    "Diagnostics" : "Tanılar",
+    "Nothing (collecting but not used)" : "Hiçbir şey (toplanıyor ama kullanılmıyor)",
+    "Summary (one report per request)" : "Özet (her istek için bir rapor)",
+    "All queries (summary, single queries with their parameters)" : "Tüm sorgular (özet, parametreleriyle tek sorgular)",
+    "All events (summary, single events)" : "Tüm olaylar (özet, tekil olaylar)",
+    "Everything (summary, single queries with their parameters and events)" : "Her şey (özet, parametreleri ve olaylarıyla birlikte tek sorgular)",
+    "Enabling this ownCloud diagnostic module will result in collecting data about all queries and events in the system per request." : "Bu ownCloud tanılama modülünün etkinleştirilmesi, istek üzerine sistemdeki tüm sorgular ve olaylar hakkında veri toplanmasına neden olur.",
+    "Collect data only after authentication of users:" : "Yalnızca kullanıcıların kimlik doğrulamasından sonra veri toplayın:",
+    "Please specify full user name for best search performance" : "En iyi performams için lütfen tam kullanıcı adını belirtin",
+    "Allow collecting data for all requests in debug mode (all users, unauthenticated requests)" : "Hata ayıklama modundaki tüm istekler için veri toplanmasına izin ver (tüm kullanıcılar, kimliği doğrulanmamış istekler)",
+    "What to log" : "Neler günlüklenmeli",
+    "Decide what details should be included in the log file" : "Günlük dosyasına hangi detayların dahil edileceğine karar verin",
+    "Diagnostic Log" : "Tanı günlüğü",
+    "Download logfile (%s)" : "Günlük dosyasını indir (%s)",
+    "Clean logfile" : "Günlük dosyasını temizle",
+    "Log file is located by default in ./data/diagnostic.log" : "Günlük dosyası öntanımlı olarak ./data/diagnostic.log konumundadır",
+    "The logfile is empty!" : "Günlük dosyası boş!",
+    "The logfile is bigger than 100 MB. Downloading it may take some time!" : "Günlük dosyası 100 MB'dan daha büyük. İndirmek zaman alabilir!"
+},
+"nplurals=2; plural=(n > 1);");
