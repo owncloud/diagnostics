ae7647268003a3978116efc20b27a045bd2cf577
Thomas Müller <thomas.mueller@tmit.eu>
2020-10-31 00:05:27 +0000

    [tx] updated from transifex

---
+++ l10n/de_CH.js
@@ -0,0 +1,24 @@
+OC.L10N.register(
+    "diagnostics",
+    {
+    "Users" : "Benutzer",
+    "Diagnostics" : "Diagnose",
+    "Nothing (collecting but not used)" : "Nichts (gesammelt aber nicht genutzt)",
+    "Summary (one report per request)" : "Zusammenfassung (eine Auswertung pro Anfrage)",
+    "All queries (summary, single queries with their parameters)" : "Alle Abfragen (Zusammenfassung, einzelne Abfragen mit deren Parameter und Ereignissen)",
+    "All events (summary, single events)" : "Alle Ereignisse (Zusammenfassung, einzelne Ereignisse)",
+    "Everything (summary, single queries with their parameters and events)" : "Alles (Zusammenfassung, einzelne Abfragen mit deren Parameter und Ereignissen)",
+    "Enabling this ownCloud diagnostic module will result in collecting data about all queries and events in the system per request." : "Wenn dieses ownCloud-Modul aktiviert ist, werden Daten zu allen Abfragen und Ereignissen im System auf Anfrage hin gesammelt.",
+    "Collect data only after authentication of users:" : "Daten nur nach Authentifizierung der Benutzer sammeln:",
+    "Please specify full user name for best search performance" : "Für beste Leistung bitte einen kompletten Benutzer angeben",
+    "Allow collecting data for all requests in debug mode (all users, unauthenticated requests)" : "Erlaube Sammeln der Daten aller Anfragen im Debug-Modus (alle Nutzer, nicht authorisierte Anfragen)",
+    "What to log" : "Was für ein Protokoll",
+    "Decide what details should be included in the log file" : "Welche Details im Protokoll gespeichert werden",
+    "Diagnostic Log" : "Diagnoseprotokoll",
+    "Download logfile (%s)" : "Logdatei (%s) herunterladen",
+    "Clean logfile" : "Protokollierung aufräumen",
+    "Log file is located by default in ./data/diagnostic.log" : "Die Protokolldatein wird standardmässig in ./data/diagnostic.log gespeichert",
+    "The logfile is empty!" : "Das Protokoll ist leer",
+    "The logfile is bigger than 100 MB. Downloading it may take some time!" : "Die Logdatei ist grösser als 100 MB. Es kann etwas Zeit beanspruchen, sie herunterzuladen!"
+},
+"nplurals=2; plural=(n != 1);");
