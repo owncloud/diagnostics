e2585aa7b0ca89d22652e378a0ad11299096d4fc
ownClouders <devops@owncloud.com>
2022-10-01 00:06:52 +0000

    [tx] updated from transifex

---
+++ l10n/oc.js
@@ -0,0 +1,22 @@
+OC.L10N.register(
+    "diagnostics",
+    {
+    "Users" : "Utilizaires",
+    "Diagnostics" : "Diagnostics",
+    "Nothing (collecting but not used)" : "Arren (collectat mès pas utilizat)",
+    "Summary (one report per request)" : "Somari (un rapòrt per requesta)",
+    "All queries (summary, single queries with their parameters)" : "Totas las requestas (somari, requestas simplas d'amb lors paramètres)",
+    "All events (summary, single events)" : "Tots los eveniments (somari, eveniments simples)",
+    "Everything (summary, single queries with their parameters and events)" : "Tot (somari, requestas simplas d'amb lors paramètres e eveniments)",
+    "Enabling this ownCloud diagnostic module will result in collecting data about all queries and events in the system per request." : "Autorizar aquest module de diagnostic resultara dens la collècta de donadas sus las requestas e los evenimentse dens lo sistema per requesta",
+    "Collect data only after authentication of users:" : "Collectar las donadas solament après l'autentificacion de l'utilizador",
+    "Lock the diagnostic.log file while writing. Useful for HA setups with NFS for the local storage" : "Clavar lo fichèr de log mentre enregistrar. Es util per las configuracions de HA d'amb NFS per lo systema de fichèr locau",
+    "What to log" : "Que enregistrar",
+    "Diagnostic Log" : "Fichèr de diagnostic",
+    "Download logfile (%s)" : "Telecargar lo fichèr de log ( %s )",
+    "Clean logfile" : "Netejar lo fichèr de log",
+    "Log file is located by default in ./data/diagnostic.log" : "Lo fichèr de log se troba per defaut dens  ./data/diagnostic.log",
+    "The logfile is empty!" : "Lo fichèr de log es viud",
+    "The logfile is bigger than 100 MB. Downloading it may take some time!" : "La talha del fichièr jornal excedís 100 Mo. Lo telecargar pòt prene un certan temps!"
+},
+"nplurals=2; plural=(n > 1);");
