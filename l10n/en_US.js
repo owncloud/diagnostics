0820f33b95e5602739d42709de845217e455fc59
ownClouders <devops@owncloud.com>
2025-12-02 00:06:42 +0000

    [tx] updated from transifex

---
+++ l10n/en_US.js
@@ -0,0 +1,22 @@
+OC.L10N.register(
+    "diagnostics",
+    {
+    "Diagnostics" : "Diagnostics",
+    "Nothing (collecting but not used)" : "Nothing (collecting but not used)",
+    "Summary (one report per request)" : "Summary (one report per request)",
+    "All queries (summary, single queries with their parameters)" : "All queries (summary, single queries with their parameters)",
+    "All events (summary, single events)" : "All events (summary, single events)",
+    "Everything (summary, single queries with their parameters and events)" : "Everything (summary, single queries with their parameters and events)",
+    "Enabling this ownCloud diagnostic module will result in collecting data about all queries and events in the system per request." : "Enabling this ownCloud diagnostic module will result in collecting data about all queries and events in the system per request.",
+    "Collect data only after authentication of users:" : "Collect data only after authentication of users:",
+    "Please specify full user name for best search performance" : "Please specify full user name for best search performance",
+    "Allow collecting data for all requests in debug mode (all users, unauthenticated requests)" : "Allow collecting data for all requests in debug mode (all users, unauthenticated requests)",
+    "Lock the diagnostic.log file while writing. Useful for HA setups with NFS for the local storage" : "Lock the diagnostic.log file while writing. Useful for HA setups with NFS for the local storage",
+    "Decide what details should be included in the log file" : "Decide what details should be included in the log file",
+    "Diagnostic Log" : "Diagnostic Log",
+    "Clean logfile" : "Clean logfile",
+    "Log file is located by default in ./data/diagnostic.log" : "Log file is located by default in ./data/diagnostic.log",
+    "The logfile is empty!" : "The logfile is empty!",
+    "The logfile is bigger than 100 MB. Downloading it may take some time!" : "The logfile is larger than 100 MB. Downloading it may take some time!"
+},
+"nplurals=2; plural=(n != 1);");
